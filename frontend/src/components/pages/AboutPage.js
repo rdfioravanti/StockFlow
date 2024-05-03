e066d6b0d54e56352ea3e79739c98da22c1f0ae2
Richard Fioravanti <rdfioravanti111@gmail.com>
2024-05-02 21:49:44 -0400

    Finished about page styling

--- frontend/src/components/pages/AboutPage.js
+++ frontend/src/components/pages/AboutPage.js
@@ -12,18 +12,18 @@ const AboutPage = () => {
       <Navbar />
       <div style={{ padding: '20px' }}>
         <h2 style={{textAlign: 'center'}}>About StockFlow</h2>
-        <div style={{textAlign: 'center', maxWidth: '600px', margin: '0 auto'}}>
+        <div style={{textAlign: 'center', maxWidth: '700px', margin: '0 auto', fontSize: '1.1rem', lineHeight: '1.6'}}>
           <p>
             StockFlow is an inventory management application developed by Richard Fioravanti.
             It was created using the SERN stack (SQL, Express, React, Node.js), with dynamic rendering from React and other technologies.
             The backend is powered by Node.js with Express, featuring custom middleware, and utilizes a PostgreSQL database.
           </p>
           <p>
-            For more information, you can visit the application's 
+            For more information, you can visit the application's {' '}
             <a href="https://github.com/rdfioravanti/StockFlow" target="_blank" rel="noopener noreferrer">GitHub</a>
           </p>
           <p>
-            Connect with Richard on 
+            Connect with Richard on {' '}
             <a href="https://www.linkedin.com/in/richard-fioravanti-6801b4275/" target="_blank" rel="noopener noreferrer">LinkedIn</a>
           </p>
         </div>
