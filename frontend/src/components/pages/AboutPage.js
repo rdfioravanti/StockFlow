88a8bdec04aa28e8e2f1d608b64bf0821b6661d6
Richard Fioravanti <rdfioravanti111@gmail.com>
2024-05-02 21:35:06 -0400

    Added about page information

--- frontend/src/components/pages/AboutPage.js
+++ frontend/src/components/pages/AboutPage.js
@@ -1,13 +1,30 @@
 import React from 'react';
 import Navbar from '../functional components/Navbar';
+import NotLoggedInPage from '../functional components/NotLoggedInRender'; // Importing the NotLoggedInPage component
 
 const AboutPage = () => {
+  if (!localStorage.getItem('idToken')) {
+    return <NotLoggedInPage />;
+  }
+
   return (
     <div>
       <Navbar />
       <div style={{ padding: '20px' }}>
-        <h2>About Page</h2>
-        {/* Your about page content goes here */}
+        <h2>About StockFlow</h2>
+        <p>
+          StockFlow is an inventory management application developed by Richard Fioravanti.
+          It was created using the SERN stack (SQL, Express, React, Node.js), with dynamic rendering from React and other technologies.
+          The backend is powered by Node.js with Express, featuring custom middleware, and utilizes a PostgreSQL database.
+        </p>
+        <p>
+          For more information, you can visit the application's GitHub repository:
+          <a href="https://github.com/rdfioravanti/StockFlow" target="_blank" rel="noopener noreferrer">GitHub</a>
+        </p>
+        <p>
+          Connect with Richard on LinkedIn:
+          <a href="https://www.linkedin.com/in/richard-fioravanti-6801b4275/" target="_blank" rel="noopener noreferrer">LinkedIn</a>
+        </p>
       </div>
     </div>
   );
